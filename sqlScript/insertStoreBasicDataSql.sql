ee58a955067f402a3f387de9cffd19ee3cfac3cc
Tomasz Murzynski <tomasz.murzynski@gmail.com>
2018-03-14 01:56:37 +0100

    To the database inserted basic categories and children of these categories

---
+++ sqlScript/insertStoreBasicDataSql.sql
@@ -0,0 +1,13 @@
+INSERT INTO `kategoria`(`nazwa`) VALUES ('Kwiaty');
+INSERT INTO `kategoria`(`nazwa`) VALUES ('Doniczki i osłonki');
+INSERT INTO `kategoria`(`nazwa`) VALUES ('Ozdoby');
+
+INSERT INTO `podkategoria`(`id_kategoria`, `nazwa`) VALUES ('1','Doniczkowe');
+INSERT INTO `podkategoria`(`id_kategoria`, `nazwa`) VALUES ('1','Cięte');
+INSERT INTO `podkategoria`(`id_kategoria`, `nazwa`) VALUES ('1','Sztuczne');
+INSERT INTO `podkategoria`(`id_kategoria`, `nazwa`) VALUES ('2','Plastikowe');
+INSERT INTO `podkategoria`(`id_kategoria`, `nazwa`) VALUES ('2','Porcelanowe');
+INSERT INTO `podkategoria`(`id_kategoria`, `nazwa`) VALUES ('2','Szklane');
+INSERT INTO `podkategoria`(`id_kategoria`, `nazwa`) VALUES ('3','Świąteczne');
+INSERT INTO `podkategoria`(`id_kategoria`, `nazwa`) VALUES ('3','Okazjonalne');
+INSERT INTO `podkategoria`(`id_kategoria`, `nazwa`) VALUES ('3','Florystyczne');
