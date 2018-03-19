b1bb0b4008a760b72e252729ce7893e247b445e4
Tomasz Murzynski <tomasz.murzynski@gmail.com>
2018-03-19 19:56:53 +0100

    New branch TDD

---
+++ TDD/src/main/java/CheckNumber.java
@@ -0,0 +1,17 @@
+
+
+public class CheckNumber {
+
+    public CheckNumber(){
+        int number;
+    }
+
+    public static boolean isEven(int number){
+        if (number%2==0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ TDD/src/test/java/CheckNumber.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CheckNumber {
+    @Test
+    public boolean IsEven(int number){
+        Integer liczba = new Integer(10);
+        Assert.assertTrue(CheckNumber.);
+
+
+        return true;
+    }
+}
